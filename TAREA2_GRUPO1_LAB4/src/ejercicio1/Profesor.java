6d798ad55aa45d2d80b9103cc8c3366969b4a5a7
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-03 21:44:36 -0300

    Se identa el codigo de Manuel

--- TAREA2_GRUPO1_LAB4/src/ejercicio1/Profesor.java
+++ TAREA2_GRUPO1_LAB4/src/ejercicio1/Profesor.java
@@ -2,23 +2,23 @@ package ejercicio1;
 
 public class Profesor extends Empleado {
 	
-	//Atributos
-
 	private String cargo;
 	private int antiguedadDocente;
-	
+
 	public Profesor() {
 		super();
 		this.cargo = "Sin cargo";
 		this.antiguedadDocente = 0;
+		
 	}
 
 	public Profesor(String nombre, int edad, String cargo, int antiguedadDocente) {
 		super(nombre, edad);
 		this.cargo = cargo;
 		this.antiguedadDocente = antiguedadDocente;
-	}
 	
+	}
+
 	public String getCargo() {
 		return cargo;
 	}
