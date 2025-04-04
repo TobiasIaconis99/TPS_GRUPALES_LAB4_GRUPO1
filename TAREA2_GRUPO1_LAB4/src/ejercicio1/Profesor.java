10d48579738c9129784ba2638c61eaff57ea62cd
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-03 21:37:27 -0300

    Se agrega el punto 2 y 3 de Manuel

--- TAREA2_GRUPO1_LAB4/src/ejercicio1/Profesor.java
+++ TAREA2_GRUPO1_LAB4/src/ejercicio1/Profesor.java
@@ -4,7 +4,34 @@ public class Profesor extends Empleado {
 	
 	//Atributos
 
-	String cargo;
-	int antiguedadDocente;
+	private String cargo;
+	private int antiguedadDocente;
+	
+	public Profesor() {
+		super();
+		this.cargo = "Sin cargo";
+		this.antiguedadDocente = 0;
+	}
 
+	public Profesor(String nombre, int edad, String cargo, int antiguedadDocente) {
+		super(nombre, edad);
+		this.cargo = cargo;
+		this.antiguedadDocente = antiguedadDocente;
+	}
+	
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
 }
