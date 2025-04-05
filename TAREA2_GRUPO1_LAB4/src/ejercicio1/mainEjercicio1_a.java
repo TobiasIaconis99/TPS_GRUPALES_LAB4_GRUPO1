e11249c8612072b424c6e809198c6440453d0366
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-05 12:52:42 -0300

    Se termina el Ejercicio 5

--- TAREA2_GRUPO1_LAB4/src/ejercicio1/mainEjercicio1_a.java
+++ TAREA2_GRUPO1_LAB4/src/ejercicio1/mainEjercicio1_a.java
@@ -1,12 +1,27 @@
 package ejercicio1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class mainEjercicio1_a {
 
 	public static void main(String[] args) {
 		
 		ArrayList<Profesor> profesoresList = new ArrayList<Profesor>();
+		
+		
+		profesoresList.add(new Profesor("Ana", 40, "Titular", 15));
+		profesoresList.add(new Profesor("Bruno", 35, "Adjunto", 10));
+		profesoresList.add(new Profesor("Claudia", 45, "JTP", 20));
+		profesoresList.add(new Profesor("Diego", 50, "Titular", 25));
+		profesoresList.add(new Profesor("Elena", 30, "Ayudante", 5));
+		
+		
+		Iterator<Profesor> iterator = profesoresList.iterator();
+		while (iterator.hasNext()) {
+			Profesor p = iterator.next();
+			System.out.println(p.toString());
+		}
 		
 
 	}
