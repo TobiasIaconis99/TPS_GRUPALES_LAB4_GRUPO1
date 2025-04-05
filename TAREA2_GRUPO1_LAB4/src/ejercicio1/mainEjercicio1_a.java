30426593f88246cca8b120bb3f6152a3b1441052
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-05 12:30:57 -0300

    Se crea el arrayList

--- TAREA2_GRUPO1_LAB4/src/ejercicio1/mainEjercicio1_a.java
+++ TAREA2_GRUPO1_LAB4/src/ejercicio1/mainEjercicio1_a.java
@@ -1,9 +1,12 @@
 package ejercicio1;
 
+import java.util.ArrayList;
+
 public class mainEjercicio1_a {
 
 	public static void main(String[] args) {
 		
+		ArrayList<Profesor> profesoresList = new ArrayList<Profesor>();
 		
 
 	}
