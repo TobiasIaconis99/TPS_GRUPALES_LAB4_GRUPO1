a6dd3552175fdb8b3c213c8e69b7525ec4c9e2d4
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-06 18:45:31 -0300

    Cambios

--- TAREA2_GRUPO1_LAB4/src/ejercicio1/mainEjercicio1_a.java
+++ TAREA2_GRUPO1_LAB4/src/ejercicio1/mainEjercicio1_a.java
@@ -10,11 +10,11 @@ public class mainEjercicio1_a {
 		ArrayList<Profesor> profesoresList = new ArrayList<Profesor>();
 		
 		
-		profesoresList.add(new Profesor("Ana", 40, "Titular", 15));
-		profesoresList.add(new Profesor("Bruno", 35, "Adjunto", 10));
-		profesoresList.add(new Profesor("Claudia", 45, "JTP", 20));
-		profesoresList.add(new Profesor("Diego", 50, "Titular", 25));
-		profesoresList.add(new Profesor("Elena", 30, "Ayudante", 5));
+		profesoresList.add(new Profesor("Ana", 40, "Profesor titular", 15));
+		profesoresList.add(new Profesor("Bruno", 35, "Profesor adjunto", 10));
+		profesoresList.add(new Profesor("Claudia", 45, "Jefe de trabajos prácticos", 20));
+		profesoresList.add(new Profesor("Diego", 50, "Ayudante de cátedra", 25));
+		profesoresList.add(new Profesor("Elena", 30, "Profesor titular", 5));
 		
 		
 		Iterator<Profesor> iterator = profesoresList.iterator();
