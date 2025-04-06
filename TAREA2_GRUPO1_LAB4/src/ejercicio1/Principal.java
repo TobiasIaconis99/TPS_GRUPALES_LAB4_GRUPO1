e86aa2ecf0a26fccf81da2ab392e0bfd1dcabe8a
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-06 19:58:51 -0300

    Se agrega el punto 6

--- TAREA2_GRUPO1_LAB4/src/ejercicio1/Principal.java
+++ TAREA2_GRUPO1_LAB4/src/ejercicio1/Principal.java
@@ -3,7 +3,7 @@ package ejercicio1;
 public class Principal {
 
 	public static void main(String[] args) {
-		Profesor profesor = new Profesor(null, 0, null, 0);
+		Profesor profesor = new Profesor();
 		System.out.println(profesor.toString());
 
 	}
--- TAREA2_GRUPO1_LAB4/src/ejercicio1/mainEjercicio1_b.java
+++ TAREA2_GRUPO1_LAB4/src/ejercicio1/mainEjercicio1_b.java
@@ -2,17 +2,19 @@ package ejercicio1;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.TreeSet;
 
-public class mainEjercicio1_a {
+public class mainEjercicio1_b {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Profesor> profesoresList = new ArrayList<Profesor>();
+		
+		TreeSet<Profesor> profesoresList = new TreeSet<Profesor>();
 		
 		
-		profesoresList.add(new Profesor("Ana", 40, "Titular", 15));
-		profesoresList.add(new Profesor("Bruno", 35, "Adjunto", 10));
-		profesoresList.add(new Profesor("Claudia", 45, "JTP", 20));
+		profesoresList.add(new Profesor("Pepe", 40, "Titular", 15));
+		profesoresList.add(new Profesor("Marilu", 35, "Adjunto", 10));
+		profesoresList.add(new Profesor("Camila", 45, "JTP", 20));
 		profesoresList.add(new Profesor("Diego", 50, "Titular", 25));
 		profesoresList.add(new Profesor("Elena", 30, "Ayudante", 5));
 		
@@ -22,7 +24,6 @@ public class mainEjercicio1_a {
 			Profesor p = iterator.next();
 			System.out.println(p.toString());
 		}
-
 
 	}
 
