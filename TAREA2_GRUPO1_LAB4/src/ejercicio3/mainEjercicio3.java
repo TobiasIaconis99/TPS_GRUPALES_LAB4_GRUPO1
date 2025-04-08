660cd09bd4dca1ff6d234e7209efc7bba1857bce
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-08 20:03:25 -0300

    Se agrega el punto de David ejercicio 3

---
+++ TAREA2_GRUPO1_LAB4/src/ejercicio3/mainEjercicio3.java
@@ -0,0 +1,25 @@
+package ejercicio3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class mainEjercicio3 {
+
+	public static void main(String[] args) {
+
+
+        ArrayList<IEdificio> edificios = new ArrayList<>();
+
+    
+        edificios.add(new Polideportivo("Polideportivo San Martín", 1500.0, 1));
+        edificios.add(new Polideportivo("Polideportivo Belgrano", 1200.0, 2));
+        edificios.add(new Polideportivo("Polideportivo Roca", 1800.0, 1));
+
+    
+        edificios.add(new Edificio(15, 3000.0));
+        edificios.add(new Edificio(25, 4500.0));
+        Iterator<IEdificio> iterator = edificios.iterator();
+
+	}
+
+}
