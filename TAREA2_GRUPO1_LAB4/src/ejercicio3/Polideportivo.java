7583dde172ec927119e162e268bd69af1b215c7b
ManuelChique <chiquemanuel@gmail.com>
2025-04-08 16:00:01 -0300

    Clase polideportivo

---
+++ TAREA2_GRUPO1_LAB4/src/ejercicio3/Polideportivo.java
@@ -0,0 +1,29 @@
+package ejercicio3;
+
+public class Polideportivo implements instalacionDeportiva, IEdificio {
+    private String nombre;
+    private double superficie;
+    private int tipoInstalacion;
+
+    public Polideportivo(String nombre, double superficie, int tipoInstalacion) {
+        this.nombre = nombre;
+        this.superficie = superficie;
+        this.tipoInstalacion = tipoInstalacion;
+    }
+
+    @Override
+    public int getTipoDeInstalacion() {
+        return tipoInstalacion;
+    }
+
+    @Override
+    public double getSuperficieEdificio() {
+        return superficie;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+}
