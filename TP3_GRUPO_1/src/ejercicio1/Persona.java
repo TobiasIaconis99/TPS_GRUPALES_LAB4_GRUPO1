bf5ec2df261c37adcc72949295c8d6cf307c6b1f
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-16 01:35:52 -0300

    Ejercicio 2

--- TP3_GRUPO_1/src/ejercicio1/Persona.java
+++ TP3_GRUPO_1/src/ejercicio1/Persona.java
@@ -1,56 +1,68 @@
 package ejercicio1;
 
-public class Persona {
-private String Nombre;
-private String Apellido;
-private String DNI;
+public class Persona implements Comparable<Persona> {
+	private String Nombre;
+	private String Apellido;
+	private String DNI;
+	
+	public Persona() {
+	
+	}
+	
+	public Persona(String Nombre, String Apellido, String DNI) {
+		this.Nombre = Nombre;
+		this.Apellido = Apellido;
+		this.DNI = DNI;
+	}
+	
+	public boolean verificarDniInvalido(String dni) throws DniInvalido {
+		boolean b = true;
+		for (int i = 0; i < dni.length(); i++) {
+			if(!Character.isDigit(dni.charAt(i))) {
+				b = false;
+			}
+		}
+		
+		if(!b) {
+			throw new DniInvalido();
+		}
+		else {
+			return true;
+		}
+	}
 
-public Persona() {
+	public String getNombre() {
+		return Nombre;
+	}
 
-}
-
-public Persona(String Nombre, String Apellido, String DNI) {
-this.Nombre = Nombre;
-this.Apellido = Apellido;
-this.DNI = DNI;
-}
-
-public boolean verificarDniInvalido(String dni) throws DniInvalido {
-    for (int i = 0; i < dni.length(); i++) {
-        if (!Character.isDigit(dni.charAt(i))) {
-            throw new DniInvalido();
-        }
-    }
-    return false;
-}
-
-public String getNombre() {
-return Nombre;
-}
-
-public void setNombre(String nombre) {
-Nombre = nombre;
-}
-
-public String getApellido() {
-return Apellido;
-}
-
-public void setApellido(String apellido) {
-Apellido = apellido;
-}
-
-public String getDNI() {
-return DNI;
-}
-
-public void setDNI(String dNI) {
-DNI = dNI;
-}
-
-@Override
-public String toString() {
-return "Persona [Nombre=" + Nombre + ", Apellido=" + Apellido + ", DNI=" + DNI + "]";
-}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return Apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	
+	public String getDNI() {
+		return DNI;
+	}
+	
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+	
+	@Override
+	public String toString() {
+		return "Persona [Nombre=" + Nombre + ", Apellido=" + Apellido + ", DNI=" + DNI + "]";
+	}
+	
+	@Override
+	public int compareTo(Persona o) {
+		return this.Apellido.compareTo(o.Apellido);
+	}
 
 }
--- TP3_GRUPO_1/src/ejercicio1/Principal.java
+++ TP3_GRUPO_1/src/ejercicio1/Principal.java
@@ -1,9 +1,20 @@
 package ejercicio1;
 
+import java.util.TreeSet;
+
 public class Principal {
 
 	public static void main(String[] args) {
-
+		
+		Archivo archivo = new Archivo();	
+		archivo.setRuta("Personas.txt");
+		TreeSet<Persona> tsPersonas = new TreeSet<Persona>();
+		if(archivo.existe())
+		{
+			archivo.leer_a_TreeSet(tsPersonas);
+			
+			
+		}
 	}
 
 }
