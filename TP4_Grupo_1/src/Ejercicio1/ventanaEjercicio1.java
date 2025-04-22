61adb7700d0f2128acbf137b87a6a19e0c1b2198
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-04-22 14:28:29 -0300

    Se agregan todos los controles a la ventana ejercicio1

--- TP4_Grupo_1/src/Ejercicio1/ventanaEjercicio1.java
+++ TP4_Grupo_1/src/Ejercicio1/ventanaEjercicio1.java
@@ -5,11 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ventanaEjercicio1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JTextField txtNombre;
+	private JTextField txtApellido;
+	private JTextField txtTelefono;
+	private JTextField txtFechaNacimiento;
 
 	/**
 	 * Launch the application.
@@ -32,11 +40,56 @@ public class ventanaEjercicio1 extends JFrame {
 	 */
 	public ventanaEjercicio1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
+		setBounds(100, 100, 414, 326);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(45, 40, 46, 14);
+		getContentPane().add(lblNombre);
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(157, 34, 168, 20);
+		getContentPane().add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JLabel lblApellido = new JLabel("Apellidos");
+		lblApellido.setBounds(45, 77, 46, 14);
+		getContentPane().add(lblApellido);
+		
+		txtApellido = new JTextField();
+		txtApellido.setBounds(157, 71, 168, 20);
+		getContentPane().add(txtApellido);
+		txtApellido.setColumns(10);
+		
+		JLabel lblTelefono = new JLabel("Telefono");
+		lblTelefono.setBounds(45, 118, 46, 14);
+		getContentPane().add(lblTelefono);
+		
+		txtTelefono = new JTextField();
+		txtTelefono.setBounds(157, 112, 168, 20);
+		getContentPane().add(txtTelefono);
+		txtTelefono.setColumns(10);
+		
+		JLabel lblFechaNacimiento = new JLabel("Fecha nacimiento");
+		lblFechaNacimiento.setBounds(46, 161, 91, 14);
+		getContentPane().add(lblFechaNacimiento);
+		
+		txtFechaNacimiento = new JTextField();
+		txtFechaNacimiento.setBounds(156, 155, 169, 20);
+		getContentPane().add(txtFechaNacimiento);
+		txtFechaNacimiento.setColumns(10);
+		
+		JLabel lblDatosIngresados = new JLabel("Los datos ingresados fueron:");
+		lblDatosIngresados.setBounds(32, 249, 183, 14);
+		getContentPane().add(lblDatosIngresados);
+		
+		JButton btnMostrar = new JButton("Mostrar");
+		btnMostrar.setBounds(236, 198, 89, 23);
+		getContentPane().add(btnMostrar);
+		
+		JButton btnMostrarInfo = new JButton("Mostrar");
+		btnMostrarInfo.setBounds(242, 198, 89, 23);
+		
 	}
 	public void cambiarVisibilidad(boolean estado) {
 		setVisible(estado);
