bbb6d45b3b846035f992f6028576d7b4e3d8151c
NicolasMontesano <nicojm197@gmail.com>
2025-04-22 12:16:02 -0300

    se sube el tp 4

---
+++ TP4_Grupo_1/src/Ejercicio1/Principal.java
@@ -0,0 +1,11 @@
+package Ejercicio1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		VentanaPrincipal ventana = new VentanaPrincipal();
+		
+		ventana.cambiarVisibilidad(true);
+	}
+
+}
---
+++ TP4_Grupo_1/src/Ejercicio1/VentanaPrincipal.java
@@ -0,0 +1,74 @@
+package Ejercicio1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal frame = new VentanaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnEjercicio2 = new JButton("Ejercicio 2");
+		btnEjercicio2.setBounds(165, 139, 142, 21);
+		contentPane.add(btnEjercicio2);
+		
+		JButton btnEjercicio1 = new JButton("Ejercicio 1");
+		btnEjercicio1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ventanaEjercicio1 ventana = new ventanaEjercicio1();
+				
+				ventana.cambiarVisibilidad(true);
+			}
+		});
+		btnEjercicio1.setBounds(165, 108, 142, 21);
+		contentPane.add(btnEjercicio1);
+		
+		JButton btnEjercicio3 = new JButton("Ejercicio 3");
+		btnEjercicio3.setBounds(165, 170, 142, 21);
+		contentPane.add(btnEjercicio3);
+		
+		JLabel lblGrupoNro1 = new JLabel("Grupo NRO: 1");
+		lblGrupoNro1.setBounds(93, 44, 83, 21);
+		contentPane.add(lblGrupoNro1);
+	}
+	
+	public void cambiarVisibilidad(boolean estado) {
+		setVisible(estado);
+	}
+}
