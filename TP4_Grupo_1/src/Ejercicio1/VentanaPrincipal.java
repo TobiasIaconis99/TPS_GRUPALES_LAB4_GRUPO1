964c1c495ab859fbea5109ab88d210d9ff0eab6b
ManuelChique <chiquemanuel@gmail.com>
2025-04-24 22:26:21 -0300

    Pequeño Arreglo

--- TP4_Grupo_1/src/Ejercicio1/VentanaPrincipal.java
+++ TP4_Grupo_1/src/Ejercicio1/VentanaPrincipal.java
@@ -16,25 +16,6 @@ public class VentanaPrincipal extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	/*public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaPrincipal frame = new VentanaPrincipal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
-
-	/**
-	 * Create the frame.
-	 */
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
