ff829bf8c2bf6c2fb949c856fd42e37634f9a087
Nicolas Veron <nicolasveron255@gmail.com>
2025-04-26 20:37:57 -0300

    Dibujar la parte de contador
    Lebel y text de contador.
    Bordes de paneles.
    Acomodo de diseño.

--- TP4_Grupo_1/src/Ejercicio1/VentanaEjercicio3.java
+++ TP4_Grupo_1/src/Ejercicio1/VentanaEjercicio3.java
@@ -5,8 +5,14 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
 public class VentanaEjercicio3 extends JFrame {
+	private JTextField textField;
 
 	public VentanaEjercicio3() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -15,33 +21,35 @@ public class VentanaEjercicio3 extends JFrame {
 		getContentPane().setLayout(null);
 		
 		JPanel panelSistOperativo = new JPanel();
-		panelSistOperativo.setBounds(8, 34, 420, 44);
+		panelSistOperativo.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelSistOperativo.setBounds(28, 34, 377, 44);
 		getContentPane().add(panelSistOperativo);
 		panelSistOperativo.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Elije un Sistema Operativo:");
-		lblNewLabel.setBounds(8, 10, 168, 24);
+		lblNewLabel.setBounds(10, 10, 160, 24);
 		panelSistOperativo.add(lblNewLabel);
 		
 		JRadioButton rdbtnWindows = new JRadioButton("Windows");
-		rdbtnWindows.setBounds(180, 12, 96, 21);
+		rdbtnWindows.setBounds(176, 12, 84, 21);
 		panelSistOperativo.add(rdbtnWindows);
 		
 		JRadioButton rdbtnMac = new JRadioButton("Mac");
-		rdbtnMac.setBounds(276, 12, 70, 21);
+		rdbtnMac.setBounds(262, 12, 51, 21);
 		panelSistOperativo.add(rdbtnMac);
 		
 		JRadioButton rdbtnLinux = new JRadioButton("Linux");
-		rdbtnLinux.setBounds(346, 12, 70, 21);
+		rdbtnLinux.setBounds(315, 12, 56, 21);
 		panelSistOperativo.add(rdbtnLinux);
 		
 		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel.setBounds(28, 99, 377, 154);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblEspecialidad = new JLabel("Elije una Especialidad:");
-		lblEspecialidad.setBounds(8, 71, 163, 21);
+		lblEspecialidad.setBounds(10, 71, 163, 21);
 		panel.add(lblEspecialidad);
 		
 		JCheckBox chckbxProgramación = new JCheckBox("Programación");
@@ -55,6 +63,20 @@ public class VentanaEjercicio3 extends JFrame {
 		JCheckBox chckbxDiseoGrafico = new JCheckBox("Diseño Grafico");
 		chckbxDiseoGrafico.setBounds(175, 113, 172, 21);
 		panel.add(chckbxDiseoGrafico);
+		
+		JPanel panelHoras = new JPanel();
+		panelHoras.setBounds(28, 260, 377, 110);
+		getContentPane().add(panelHoras);
+		panelHoras.setLayout(null);
+		
+		JLabel lblCantHorasCompu = new JLabel("Cantidad de horas en el computador:");
+		lblCantHorasCompu.setBounds(10, 25, 230, 14);
+		panelHoras.add(lblCantHorasCompu);
+		
+		textField = new JTextField();
+		textField.setBounds(250, 22, 86, 20);
+		panelHoras.add(textField);
+		textField.setColumns(10);
 	}
 	
     public void cambiarVisibilidad(boolean estado) {
