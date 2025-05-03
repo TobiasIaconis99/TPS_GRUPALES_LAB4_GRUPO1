02cc1435bcc29d835dcf73a9d29edf01d6d018d8
David Ysaguirre <ysaguirredavid@gmail.com>
2025-05-03 14:54:35 -0300

    ID automatico

--- TP5_GRUPO_1/src/ejercicio1/Peliculas.java
+++ TP5_GRUPO_1/src/ejercicio1/Peliculas.java
@@ -1,36 +1,56 @@
 package ejercicio1;
 
 public class Peliculas {
-	
-	// Atributos
-    private int id;
+
+    // Atributos
+    private final int id;
     private String nombre;
     private Categorias categoria;
+    private static int cont = 0;
 
-    // Constructor
-    public Peliculas (String nombre, Categorias categoria) {
+    // Constructores
+    public Peliculas() {
+        cont++;
+        id = cont;
+        nombre = "Sin nombre";
+        categoria = null; 
+    }
 
+    public Peliculas(String nombre, Categorias categoria) {
+        cont++;
+        id = cont;
         this.nombre = nombre;
         this.categoria = categoria;
     }
-    
-    // Getters y setters
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public Categorias getCategoria() {
-		return categoria;
-	}
-	public void setCategoria(Categorias categoria) {
-		this.categoria = categoria;
-	}
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Categorias getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categorias categoria) {
+        this.categoria = categoria;
+    }
+
+    // Métodos
+    @Override
+    public String toString() {
+        return "PELICULA ID=" + id + ", NOMBRE=" + nombre + ", CATEGORIA=" + categoria;
+    }
+
+    public static int devuelveProximoID() {
+        return cont + 1;
+    }
 }
