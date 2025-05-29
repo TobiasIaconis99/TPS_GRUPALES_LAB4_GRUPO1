de32d8420271acdd181d20effdc4ad8f09d1a36c
Nicolas Veron <nicolasveron255@gmail.com>
2025-05-28 23:01:54 -0300

    Creacion de Proyecto TP7

---
+++ TP7_JSP_SERVLET/src/main/java/modelo/TipoSeguro.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class TipoSeguro {
+	
+	private int idTipo;
+    private String descripcion;
+    
+    public TipoSeguro(int idTipo, String descripcion) {
+        this.idTipo = idTipo;
+        this.descripcion = descripcion;
+    }
+
+	public int getIdTipo() {
+		return idTipo;
+	}
+
+	public void setIdTipo(int idTipo) {
+		this.idTipo = idTipo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoSeguro [idTipo=" + idTipo + ", descripcion=" + descripcion + "]";
+	}
+}
