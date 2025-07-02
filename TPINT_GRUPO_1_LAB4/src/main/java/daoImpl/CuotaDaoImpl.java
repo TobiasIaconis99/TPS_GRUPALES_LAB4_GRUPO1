48648c84f934c45b953b41c13e3a8703f9c89aca
Tobias Nicolas Iaconis <saibot_iaconis99@outlook.com>
2025-07-02 19:57:51 -0300

    Se las respectivas clases e interfaces de cuota en dao y negocio

---
+++ TPINT_GRUPO_1_LAB4/src/main/java/daoImpl/CuotaDaoImpl.java
@@ -0,0 +1,43 @@
+package daoImpl;
+
+import java.util.List;
+
+import dao.CuotaDao;
+import entidad.Cuota;
+import entidad.Usuario;
+
+public class CuotaDaoImpl implements CuotaDao {
+
+	@Override
+	public List<Cuota> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean agregar(Usuario u) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modificar(Usuario u) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int idUsuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Cuota> obtenerCuotasPorIdPrestamo(int idPrestamo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
